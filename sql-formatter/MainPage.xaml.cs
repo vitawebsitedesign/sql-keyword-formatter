876384fe72e82d7209b10205e3db48339041e7f6
Michael Nguyen <mn@3dsafety.com.au>
2019-08-04 19:58:03 +1000

    initial commit;

---
+++ sql-formatter/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace sql_formatter
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void TbRaw_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var raw = ((TextBox)sender).Text;
+            var formatted = await SqlFormatter.GetCasedSql(raw);
+            tbFormatted.Text = formatted;
+        }
+    }
+}

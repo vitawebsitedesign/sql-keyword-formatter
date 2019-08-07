3494179894e99f2659860a316d971f1c37feb3f5
Michael Nguyen <mn@3dsafety.com.au>
2019-08-07 21:45:03 +1000

    add tests, refactor, show replacement count and verify result

---
+++ SqlKeywordFormatter.Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SqlKeywordFormatter.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+    }
+}
---
+++ SqlKeywordFormatter.Util/SqlFormatter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SqlKeywordFormatter.Util
+{
+    public static class SqlFormatter
+    {
+        public static SqlFormatterResult Format(string keywordsRegex, string input)
+        {
+            var pattern = $@"\b({keywordsRegex})\b";
+            var commentRanges = GetCommentRanges(input);
+            var matches = Regex.Matches(input, pattern, RegexOptions.IgnoreCase).Cast<Match>();
+            var matchesToReplace = matches.Where(m => !IsMatchInComment(m.Index, commentRanges));
+
+            var sb = new StringBuilder(input);
+            foreach (var match in matchesToReplace)
+            {
+                sb.Remove(match.Index, match.Length).Insert(match.Index, match.Value.ToUpper());
+            }
+
+            var output = sb.ToString();
+            var changed = input != output;
+            var verified = input.Equals(output, StringComparison.OrdinalIgnoreCase);
+            return new SqlFormatterResult(matches.Count(), output, changed, verified);
+        }
+
+        private static IReadOnlyCollection<Range> GetCommentRanges(string sql)
+        {
+            var pattern = @"(\/\*(.|\s)+?\*\/)|(--(.|\w)+?\r)";
+            var matches = Regex.Matches(sql, pattern, RegexOptions.IgnoreCase).Cast<Match>().ToList();
+            return matches
+                .Select(Range.Map)
+                .ToList()
+                .AsReadOnly();
+        }
+
+        private static bool IsMatchInComment(int matchIndex, IReadOnlyCollection<Range> ranges)
+        {
+            foreach (var range in ranges)
+            {
+                if (matchIndex >= range.Start && matchIndex < range.End)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
